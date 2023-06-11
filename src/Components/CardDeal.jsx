f34e65e4437fe34a77cf96cb1b93a96f820eb873
kanishkasd <60418548+kanishkasd@users.noreply.github.com>
2023-06-11 08:57:16 +0530

    Add files via upload

---
+++ src/Components/CardDeal.jsx
@@ -0,0 +1,25 @@
+import { card } from "../assets";
+import styles, { layout } from "../styles";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="hidden sm:block" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Arcu tortor, purus in mattis at se integer faucibus. Aliquet quis.
+        Aliquet egt mauris tortor ac aliquet altrices ac, ametau.{" "}
+      </p>
+      <Button styles="mt-10" />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
---
+++ src/Components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Business from "./Business";
+import Billing from "./Billing";
+import Footer from "./Footer";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Hero from "./Hero";
+import GetStarted from "./GetStarted";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+  Navbar,
+  Business,
+  Billing,
+  Footer,
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  FeedbackCard,
+  Hero,
+  GetStarted,
+  Stats,
+  Testimonials,
+};
